12e44a9452340cd8edceb8e2b0ce4e0d59abddca
Steven <stzheng716@gmail.com>
2023-04-12 12:53:17 -0700

    add getImage function

--- tvmaze.js
+++ tvmaze.js
@@ -3,7 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
-const BASE_TVMAZE_URL = "http://api.tvmaze.com/";
+const BASE_TVMAZE_URL = "https://api.tvmaze.com/";
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -15,41 +15,29 @@ const BASE_TVMAZE_URL = "http://api.tvmaze.com/";
 async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return await axios.get(`${BASE_TVMAZE_URL}shows`, { params: { q: term } });
+  const reponse = await axios.get(`${BASE_TVMAZE_URL}shows`, { params: { q: term } });
+  return reponse.data;
 }
 
-// [
-//   {
-//     id: 1767,
-//     name: "The Bletchley Circle",
-//     summary: `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-//          women with extraordinary skills that helped to end World War II.</p>
-//        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-//          normal lives, modestly setting aside the part they played in
-//          producing crucial intelligence, which helped the Allies to victory
-//          and shortened the war. When Susan discovers a hidden code behind an
-//          unsolved murder she is met by skepticism from the police. She
-//          quickly realises she can only begin to crack the murders and bring
-//          the culprit to justice with her former friends.</p>`,
-//     image:
-//       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg",
-//   },
-// ];
 
 /** Given list of shows, create markup for each and append to DOM.
  *
  * A show is {id, name, summary, image}
  * */
 
-function displayShows(shows) {
+async function displayShows(shows) {
   $showsList.empty();
 
+
   for (const show of shows) {
+    
+    const imageURL = await getImage(show.id)
+
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src="${imageURL}"
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
@@ -65,6 +53,18 @@ function displayShows(shows) {
 
     $showsList.append($show);
   }
+}
+
+async function getImage(showID){
+  const showsImages =  await axios.get(`${BASE_TVMAZE_URL}shows/${showID}/images`);
+  const showsImagesURL = showsImages.data[0].resolutions.medium.url;
+  console.log(showsImagesURL)
+  if(showsImagesURL === undefined || showsImagesURL === null){
+    return "https://tinyurl.com/tv-missing";
+  } else {
+    return showsImagesURL;
+  }
+
 }
 
 /** Handle search form submission: get shows from API and display.

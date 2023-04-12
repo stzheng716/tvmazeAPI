677a85e7bf5f3de8c460f9055a76b5ad5d69483b
MKim <mrn.kim07@gmail.com>
2023-04-12 12:17:25 -0700

    update getShowsByTerm function

--- tvmaze.js
+++ tvmaze.js
@@ -3,7 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
-
+const BASE_TVMAZE_URL = "http://api.tvmaze.com/";
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,29 +12,29 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  return await axios.get(`${BASE_TVMAZE_URL}shows`, { params: { q: term } });
 }
 
+// [
+//   {
+//     id: 1767,
+//     name: "The Bletchley Circle",
+//     summary: `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+//          women with extraordinary skills that helped to end World War II.</p>
+//        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+//          normal lives, modestly setting aside the part they played in
+//          producing crucial intelligence, which helped the Allies to victory
+//          and shortened the war. When Susan discovers a hidden code behind an
+//          unsolved murder she is met by skepticism from the police. She
+//          quickly realises she can only begin to crack the murders and bring
+//          the culprit to justice with her former friends.</p>`,
+//     image:
+//       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg",
+//   },
+// ];
 
 /** Given list of shows, create markup for each and append to DOM.
  *
@@ -48,9 +48,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -67,7 +67,6 @@ function displayShows(shows) {
   }
 }
 
-
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
@@ -80,11 +79,10 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
-$searchForm.on("submit", async function handleSearchForm (evt) {
+$searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
 });
-
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
